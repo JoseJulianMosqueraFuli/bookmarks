cb99ccefe75f61e24e68cede1a034a9d6ae794ee
Jose Mosquera <jojumofu@gmail.com>
2023-07-15 10:44:57 -0500

    feat: create a user_login view

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        # Check the validity of submitted data and log in a valid user or return an error message to
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Authenticated successfully")
+                else:
+                    return HttpResponse("Disabled account")
+            else:
+                return HttpResponse("Invalid credentials")
+    else:
+        form = LoginForm()
+    return render(request, "account/login.xhtml", {"form": form})
