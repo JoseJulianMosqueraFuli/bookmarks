eba7f8cef95f7bdef32f2d69e062eb2620e7322e
Jose Mosquera <jojumofu@gmail.com>
2023-07-17 15:10:16 -0500

    feat: add UserRegistrationForm in forms.py using User auth.models

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,6 +1,18 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
+
+
+class UserRegistrationForm:
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password_confirmation = forms.CharField(
+        label="Repeat password", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "email"]
