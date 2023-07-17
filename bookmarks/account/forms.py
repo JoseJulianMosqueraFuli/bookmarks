5760682c15428389ab60f434cafb0a3acb555ab1
Jose Mosquera <jojumofu@gmail.com>
2023-07-17 15:12:16 -0500

    feat: add clean_password_confirmation function in UserRegistrationForm forms.py

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -16,3 +16,9 @@ class UserRegistrationForm:
     class Meta:
         model = User
         fields = ["username", "first_name", "email"]
+
+    def clean_password_confirmation(self):
+        cd = self.cleaned_data
+        if cd["password"] != cd["password_confirmation"]:
+            raise forms.ValidationError("Password don't match")
+        return cd["password_confirmation"]
