99f3e545eba9afbc65e4e3269666d9303fa473d7
Jose Mosquera <jojumofu@gmail.com>
2023-07-17 14:34:50 -0500

    feat: create reset password views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -18,5 +18,23 @@ urlpatterns = [
         auth_views.PasswordChangeDoneView.as_view(),
         name="password_change_done",
     ),
+    path(
+        "password-reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    ),
+    path(
+        "password-reset/done",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/<uidb64>/<token>",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password-reset/complete/",
+    ),
+    path(
+        "password-reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     path("", views.dashboard, name="dashboard"),
 ]
