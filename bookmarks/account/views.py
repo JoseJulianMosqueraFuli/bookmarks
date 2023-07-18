5ae07dc47f5d3d94b3b52f6fe76fb42919fe3761
Jose Mosquera <jojumofu@gmail.com>
2023-07-18 12:45:43 -0500

    feat: add edit view and path in url

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, ProfileEditForm, UserEditForm
 from .models import Profile
 
 
@@ -43,9 +43,30 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(
                 request, "account/register_done.xhtml", {"new_user": new_user}
             )
     else:
         user_form = UserRegistrationForm()
     return render(request, "account/register.xhtml", {"user_form": user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile, data=request.POST, files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        "account/edit.xhtml",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
