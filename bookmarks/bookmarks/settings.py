b488488ca7dc0263f921410b5c9e59273f310f48
Jose Mosquera <jojumofu@gmail.com>
2023-07-20 18:14:36 -0500

    feat: add authentication with google

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -150,6 +150,7 @@ AUTHENTICATION_BACKENDS = [
     "account.authentication.EmailAuthBackend",
     "social_core.backends.facebook.FacebookOAuth2",
     "social_core.backends.twitter.TwitterOAuth",
+    "social_core.backends.google.GoogleOAuth2",
 ]
 
 SOCIAL_AUTH_FACEBOOK_KEY = os.getenv("FACEBOOK_APP_ID")
@@ -158,3 +159,19 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ["email"]  # Optional
 
 SOCIAL_AUTH_TWITTER_KEY = os.getenv("TWITTER_API_KEY")
 SOCIAL_AUTH_TWITTER_SECRET = os.getenv("TWITTER_API_SECRET")
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("GOOGLE_CLIENT_ID")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+
+SOCIAL_AUTH_PIPELINE = [
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "account.authentication.create_profile",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+]
