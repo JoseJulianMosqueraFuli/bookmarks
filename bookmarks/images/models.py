5c6302bf05f590565726533ac0043c8d43ca1315
Jose Mosquera <jojumofu@gmail.com>
2023-07-21 08:21:29 -0500

    feat: create image model

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Image(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="images_created",
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, blank=True, unique=True)
+    url = models.ImageField(max_length=2000)
+    image = models.ImageField(upload_to="images_uploaded")
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = "image"
+        indexes = [
+            models.Index(fields=["-created"]),
+        ]
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
