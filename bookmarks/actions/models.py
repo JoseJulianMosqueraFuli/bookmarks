dd65d81ec59c68aea520c6bd289e944c795a655a
Jose Mosquera <jojumofu@gmail.com>
2023-07-30 07:18:16 -0500

    feat(activity stream): create action model

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Action(models.Model):
+    user = models.ForeignKey(
+        "auth.User",
+        related_name="action",
+        on_delete=models.CASCADE,
+    )
+    verb = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["-created"]),
+        ]
+        ordering = ["-created"]
