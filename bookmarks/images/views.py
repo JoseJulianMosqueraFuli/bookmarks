bc8631442e23943ac7e845556228bbef43d05c2e
Jose Mosquera <jojumofu@gmail.com>
2023-08-04 14:24:46 -0500

    feat(redis): add sotre a ranking in redis

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -45,6 +45,7 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     total_views = r.incr(f"image: {image.id}, views")
+    r.zincrby("image_ranking", 1, image.id)
     return render(
         request,
         "images/image/detail.xhtml",
@@ -96,4 +97,18 @@ def image_list(request):
         request,
         "images/image/list.xhtml",
         {"section": "images", "images": images},
+    )
+
+
+@login_required
+def image_ranking(request):
+    image_ranking = r.zrange("image_ranking", 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+
+    return render(
+        request,
+        "images/image/ranking.xhtml",
+        {"section": "images", "most_viewed": most_viewed},
     )
