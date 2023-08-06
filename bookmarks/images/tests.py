a88f639a66554bf4347f249304656f2dc9335141
Jose Mosquera <jojumofu@gmail.com>
2023-08-06 10:15:00 -0500

    feat(images): add more test for Images views

--- bookmarks/images/tests.py
+++ bookmarks/images/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.urls import reverse
+
 from django.contrib.auth import get_user_model
 
 from images.models import Image
@@ -28,3 +30,69 @@ class ImageTests(TestCase):
         form_data = {"title": "Invalid"}
         form = ImageCreateForm(data=form_data)
         self.assertFalse(form.is_valid())
+
+
+class ImageListViewTests(TestCase):
+    def test_paginate_images(self):
+        # Create lots of test images
+        num_images = 10
+        for i in range(num_images):
+            Image.objects.create(
+                title="Test %d" % i, url="https://example.com/", user=user
+            )
+
+        response = self.client.get("/images/")
+        self.assertEqual(response.status_code, 200)
+
+        # Check paginator context variable
+        self.assertTrue(response.context["is_paginated"])
+        self.assertEqual(len(response.context["images"]), 4)  # 4 per page
+
+
+class ImageDetailViewTests(TestCase):
+    def test_image_likes(self):
+        user = get_user_model().objects.create_user(username="test", password="pass")
+        image = Image.objects.create(
+            title="Test", url="https://example.com/", user=user
+        )
+        response = self.client.post("/images/1/like/", {"id": image.id})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(image.users_like.count(), 1)
+        self.assertTrue(user in image.users_like.all())
+
+
+class ImageCreateViewTests(TestCase):
+    def test_image_create_view(self):
+        # Login as user
+        self.client.login(username="user", password="password")
+
+        # Send POST request
+        response = self.client.post(
+            reverse("image_create"),
+            {"title": "Test Title", "url": "https://test.com/image.jpg"},
+        )
+
+        # Check redirect to detail page
+        image = Image.objects.get(title="Test Title")
+        self.assertRedirects(response, image.get_absolute_url())
+
+        # Check new image created
+        self.assertEqual(Image.objects.count(), 1)
+        self.assertEqual(Image.objects.first().title, "Test Title")
+
+
+class ImageRankingViewTests(TestCase):
+    def test_ranking_view(self):
+        # Add test ranking data
+        r.zadd("image_ranking", {1: 10, 2: 30, 3: 20})  # id: ranking score
+
+        response = self.client.get(reverse("image_ranking"))
+        self.assertEqual(response.status_code, 200)
+
+        # Check ranking order
+        first_id = response.context["most_viewed"][0].id
+        self.assertEqual(first_id, 2)
+
+        second = response.context["most_viewed"][1].id
+        self.assertEqual(second, 3)
